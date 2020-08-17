9c08811e4553076807deeb042bb76e1cb6b9f28c
Sajad <sajadalipour@aut.ac.ir>
2020-08-17 21:34:02 +0430

    first base

--- React-Training/robofriends/src/App.js
+++ React-Training/robofriends/src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         </a>
       </header>
     </div>
-    )
+    );
   }
     
 }
--- React-Training/robofriends/src/index.js
+++ React-Training/robofriends/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
-
+import 'tachyons';
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <Hello greeting={'Hello'+'React Ninja'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
