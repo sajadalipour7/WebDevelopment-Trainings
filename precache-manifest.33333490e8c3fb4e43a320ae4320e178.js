a27d104d43d5d7255cee5fcb2189b5d7ba343cf7
Sajad <sajadalipour@aut.ac.ir>
2020-08-18 21:58:11 +0430

    Updates

---
+++ precache-manifest.33333490e8c3fb4e43a320ae4320e178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cebad3dc5b14d187cd42cdb29d3db2",
+    "url": "/React-Training/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/React-Training/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5faf77b3a1c645512152",
+    "url": "/React-Training/robofriends/static/css/main.925ece2b.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/React-Training/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Training/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5faf77b3a1c645512152",
+    "url": "/React-Training/robofriends/static/js/main.db0ba253.chunk.js"
+  },
+  {
+    "revision": "68545580899296ecc5bb",
+    "url": "/React-Training/robofriends/static/js/runtime-main.0b1ffff6.js"
+  }
+]);
