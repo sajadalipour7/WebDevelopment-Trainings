174e5ed256812df80e499ea5d41ee8f66a95804c
Sajad <sajadalipour@aut.ac.ir>
2020-08-24 15:48:24 +0430

    body-parser

--- NodeJsTraining/server.js
+++ NodeJsTraining/server.js
@@ -1,13 +1,33 @@
 const express=require('express');
-
+const bodyParser=require('body-parser');
 const app=express();
 
+
+
+/// Middle Ware (Important concept!)
+
+// app.use((req,res,next)=>{
+// 	console.log('<h1>Hello</h1>');
+// 	next();
+// });
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.get('/',(req,res)=>{
 	res.send("getting root");
-})
+}) 
 
 app.get('/profile',(req,res)=>{
 	res.send("getting profile");
+})
+
+app.post('/profile',(req,res)=>{
+	console.log(req.body);
+	const user={
+		name:'sally',
+		hobby:'soccer'
+	}
+	res.send('Success');
 })
 
 app.listen(3000);
