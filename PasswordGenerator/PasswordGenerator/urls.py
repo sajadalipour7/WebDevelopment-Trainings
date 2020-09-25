66010e933a58bbbbb51d8f66cebbd920f13ef47d
Sajad <sajadalipour@aut.ac.ir>
2020-09-25 13:15:05 +0330

    finished

--- PasswordGenerator/PasswordGenerator/urls.py
+++ PasswordGenerator/PasswordGenerator/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path
 from generator import views
 urlpatterns = [
-    path('',views.home),
+    path('',views.home,name='home'),
     path('generatedpassword/',views.password,name='password'),
+    path('about/',views.about,name='about')
 ]
